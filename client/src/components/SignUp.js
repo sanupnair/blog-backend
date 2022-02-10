37e2a89e272f3fc8a609af0332b0958adb64c412
sanupnair <93447500+sanupnair@users.noreply.github.com>
2022-02-10 12:12:35 +0530

    new

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -33,7 +33,7 @@ const SignUp = () => {
         if (Object.keys(formErrorValues).length === 0 && isSubmit) {
            addUsers();
         }
-    }, [formerrorValues]);
+    }, [formErrorValues]);
 
    // var [myvalue, setmyValue] = MyForm({ username: "", email: "", password: "" })
     const addUsers = () => {
