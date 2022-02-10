870c7e77bec61138cc639f10acd6c2fe306392c7
sanupnair <93447500+sanupnair@users.noreply.github.com>
2022-02-10 13:46:21 +0530

    new

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -38,10 +38,10 @@ const SignUp = () => {
    // var [myvalue, setmyValue] = MyForm({ username: "", email: "", password: "" })
     const addUsers = () => {
         console.log(formValues)
-        axios.post("/signup", formValues).then(
+        axios.post("http://localhost:3005/api/signup", formValues).then(
             (res) => {
                 alert("Successfully Created Account!!!Now you can login using registered username and password.")
-                navigate("/signin", { replace: true })
+                navigate("../signin", { replace: true })
                 console.log("in axios post" + res.data)
 
             }
