b0de488368d887df0cbcbcb6151ef2a51191b6d3
sanupnair <93447500+sanupnair@users.noreply.github.com>
2022-02-10 12:48:05 +0530

    new

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = (props) => {
           {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to='/signin' onClick={props.setlogout(1)}>Sign Out</NavBtnLink>
+          <NavBtnLink to='/signin' onClick={props.setlogout()}>Sign Out</NavBtnLink>
         </NavBtn>
       </Nav>
     </>
